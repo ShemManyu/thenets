09033b7949e6e25558f7cd6f8da7bd278303d793
Shem Manyu <smanyu48@gmail.com>
2018-08-26 19:19:03 +0300

    Added user model

---
+++ nets/database.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import app
+from datetime import datetime
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60))
+    pwdhash = db.Column(db.String())
+    email = db.Column(db.String(60))
+    created = db.Column(db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.set_password(password)
+        self.email = email
+        self.created = datetime.utcnow()
+    
+    def set_password(self, password):
+        self.pwdhash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
+
+#class Coversation(db.Model):
+#    pass
