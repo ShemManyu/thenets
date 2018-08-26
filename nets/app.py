443e76c92854a8bcb5a6b52987c58d870448507a
Shem Manyu <smanyu48@gmail.com>
2018-08-26 18:22:36 +0300

    Initial commit, Added first api routes

---
+++ nets/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, Response
+from flask_restful import Api, Resource, abort
+
+app = Flask(__name__)
+api = Api(app)
+
+chatgroups = {
+    'group1': {
+        'message1': {'from': 'user1', 'message': 'Hey guys'},
+        'message2': {'from': 'user2', 'message': 'Hey user1'}
+    },
+    'group2': {
+        'message1': {'from': 'user1', 'message': 'Hey guys too'},
+        'message2': {'from': 'user2', 'message': 'Hey user1 too'}
+    }
+}
+
+conversations = {
+    'conversation1': {
+        'message1': {'from': 'user2', 'message': 'Hey user1'},
+        'message2': {'from': 'user2', 'message': 'Hey user1'}
+    }
+}
+
+class ChatGroup(Resource):
+    def get(self, group_id):
+        #serialize group messages and return them here
+        return chatgroups[group_id]
+    
+    def post(self, group_id):
+        message = request.form['message']
+        userid = request.form['user']
+        message_id = int(max(chatgroups[group_id].keys()).lstrip('message')) + 1
+        message_id = 'message%i' %message_id
+        chatgroups[group_id][message_id] = {
+            'from': userid,
+            'message': message 
+        }
+        return chatgroups[group_id]
+
+class Conversation(Resource):
+    def get(self, conversation_id):
+        return conversations[conversation_id]
+    
+    def post(self, conversation_id):
+        message = request.form['message']
+        userid = request.form['user']
+        message_id = int(max(conversations[conversation_id].keys()).lstrip('message')) + 1
+        message_id = 'message%i' %message_id
+        conversations[conversation_id][message_id] = {
+            'message': message,
+            'from': userid
+        }
+        return conversations[conversation_id]
+
+api.add_resource(ChatGroup, '/groups/<string:group_id>')
+api.add_resource(Conversation, '/chat/<string:conversation_id>')
+
+if __name__ == "__main__":
+    app.run(debug=True)
